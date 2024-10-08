b3158bedaf42a2435ecfb633f2befac815f7c36e
trinh <trinhlbhe163893@fpt.edu.vn>
2024-10-08 22:30:06 +0700

    add test

--- SEP490_G67/MyAPI/obj/Debug/net6.0/MyAPI.AssemblyInfo.cs
+++ SEP490_G67/MyAPI/obj/Debug/net6.0/MyAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87190a3cfc87d3d59f1d9a4ab0d1ed83801a0f04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b8c249bcca45905c9e0668b6aed8b220457efb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
