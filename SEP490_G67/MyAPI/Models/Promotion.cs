d9a44ac9c8e4a444a7f8baad83fa6ab16dab3d86
Nmtuan <tuannmhe172118@fpt.edu.vn>
2024-11-06 10:35:03 +0700

    update models

--- SEP490_G67/MyAPI/Models/Promotion.cs
+++ SEP490_G67/MyAPI/Models/Promotion.cs
@@ -16,7 +16,7 @@ namespace MyAPI.Models
         public int Discount { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
-        public string Description { get; set; } = null!;
+        public string? Description { get; set; }
         public DateTime? CreatedAt { get; set; }
         public int? CreatedBy { get; set; }
         public DateTime? UpdateAt { get; set; }
